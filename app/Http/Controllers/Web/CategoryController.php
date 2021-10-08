874c7ba5cd6e688d3d9bf74dc30fdd09fe2fabe4
camnh <you@example.com>
2021-10-08 16:28:53 +0700

    My commit

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -11,51 +11,35 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function categoryView(Request $request,$category_id){
-        $category_info = Category::where('id',$category_id)->first();
+        $category = Category::find($category_id);
         $product_infos = Category::findOrFail($category_id)->products()->get(['id','name']);
-        if ($request->has(['paginate','price'])){
-            $paginate = $request->input('paginate') < 9 ? 9 : $request->input('paginate');
-            if ($request->input('price') == 'up'){  
-                $sort_by_price = 'up';
-                $types = Category::findOrFail($category_id)->types()->orderBy('price', 'asc')->paginate($paginate);
-                $types->appends(['paginate'=> $paginate,'price'=> $sort_by_price]);
-            } else {
-                $sort_by_price = 'down';
-                $types = Category::findOrFail($category_id)->types()->orderBy('price', 'desc')->paginate($paginate);
-                $types->appends(['paginate'=> $paginate,'price'=> $sort_by_price]);
-            }
-        } elseif ($request->has('paginate')){
-            $paginate = $request->input('paginate') < 9 ? 9 : $request->input('paginate');
-            $sort_by_price = 'none';
-            $types = Category::findOrFail($category_id)->types()->paginate($paginate);
-            $types->appends(['paginate'=> $paginate]);
-        } elseif ($request->has('price')){
-            $paginate = 9;
-            if ($request->input('price') == 'up'){  
-                $sort_by_price = 'up';
-                $types = Category::findOrFail($category_id)->types()->orderBy('price', 'asc')->paginate($paginate);
-                $types->appends(['price'=> $sort_by_price]);
-                // dd($types);
-            } else {
-                $sort_by_price = 'down';
-                $types = Category::findOrFail($category_id)->types()->orderBy('price', 'desc')->paginate($paginate);
-                $types->appends(['price'=> $sort_by_price]);
-            }
-        } else {
-        $sort_by_price = 'none';
-        $paginate = 9;
-        $types = Category::findOrFail($category_id)->types()->paginate(9);
 
+        $paginate = $request->query('paginate') < 9 ? 9 : $request->query('paginate');
+        $types = collect();
+
+        if ($request->query('paginate') !== null) {
+            $types = $category->types()->orderBy('price', 'asc');
         }
-        
+
+        if ($request->query('price') !== null) {
+            $types = $category->types()->orderBy('price', $request->query('price'));
+        }
+
+        $types->paginate($paginate)->appends($request->query());
+
         // dd($types);
         $breadCrumbs = [
             [
-                'name' => $category_info->name,
+                'name' => $category->name,
                 'link' => '/category/'.$category_id,
                 // 'link' => '#'
             ],
         ];
-        return view('frontend.product',compact(['breadCrumbs','types','category_info','product_infos','paginate','sort_by_price']));
+        return view(
+            'frontend.product',
+            array_merge(compact(['breadCrumbs', 'types', 'category_info', 'product_infos', 'paginate', 'sort_by_price']), [
+                'category_info' => $category
+            ])
+        );
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,13 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'name',
         'desc',
     ];
-    public function products(){
+
+    public function products()
+    {
         return $this->hasMany(Product::class,'category_id');
     }
+
     public function types()
     {
         return $this->hasManyThrough(Type::class, Product::class);
